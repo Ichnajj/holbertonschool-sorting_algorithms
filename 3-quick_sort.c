0d01fe4bcafc4dc0bbed428c691a86adb2b3d8a7
ichnajj <10025@holbertonstudents.com>
2025-01-10 14:21:49 +0100

    selection

---
+++ 3-quick_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+* quick_sort - sorts an array of integers using the Quick sort algorithm.
+* @array: The array of integers.
+* @size: The size of the array.
+*/
+
+void quick_sort(int *array, size_t size)
+{
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+holberton sorting algorithms project
